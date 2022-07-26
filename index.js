c0a7d0a46edc5b81793bae648a67fbfc11878fb9
Taaseen <taas2002@students.ubc.ca>
2022-07-25 22:13:55 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'; 
+import bodyParser from 'body-parser';
+
+import usersRoutes from './routes/users.js';
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+
+// all routes here are starting with /users
+app.use('/users', usersRoutes);
+
+app.get('/', (req, res) => res.send('Hello from Homepage'));
+
+app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`));
